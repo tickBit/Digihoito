b38774c292f066af1feca4246e1bd10a705c76c7
tickBit <marjukosk@gmail.com>
2026-03-02 05:16:46 +0200

    First version of register user

---
+++ frontend/src/Components/Register.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import axios from 'axios';
+
+import { useAuth } from '../auth/useAuth';
+
+const Register = () => {
+
+    const { login } = useAuth();
+       
+    const handleRegister = (e: React.SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get('email') as string;
+        const password = formData.get('password') as string;
+        const confirmPassword = formData.get('confirmPassword') as string;
+        
+        if (password !== confirmPassword) {
+            alert('Passwords do not match');
+            return;
+        }
+ 
+        // Make API call to register the user
+        axios.post('/register', { email, password })
+            .then(response => {
+                alert('Registration successful');
+                
+                login(email, response.data.token);
+            })
+            .catch(error => {
+                alert('Registration failed: ' + error.message);
+            });
+        
+    }
+              
+    return (
+        <div className="register-login">
+            <h1>Register</h1>
+            <form onSubmit={handleRegister}>
+                <input type="text" name="email" placeholder="Email" required />
+                <input type="password" name="password" placeholder="Password" required />
+                <input type="password" name="confirmPassword" placeholder="Confirm Password" required />
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
+            
+}
+
+export default Register;
