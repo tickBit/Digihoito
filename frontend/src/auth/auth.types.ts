aa973f87df7c51d1c5cdba10cd43005d647d547a
tickBit <marjukosk@gmail.com>
2026-05-02 23:38:15 +0300

    Add role

--- frontend/src/auth/auth.types.ts
+++ frontend/src/auth/auth.types.ts
@@ -2,7 +2,7 @@ export interface AuthContextType {
   isLoggedIn: boolean;
   userEmail: string | null;
   token: string | null;
-  userId: string;
-  login: (email: string, token: string) => void;
+  userRole: number | null;
+  login: (email: string, token: string, role: number) => void;
   logout: () => void;
 }
