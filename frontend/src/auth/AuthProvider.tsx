32f56924ef284ca2de65ee23e1b5b02984aa1293
tickBit <marjukosk@gmail.com>
2026-05-02 23:38:35 +0300

    Handle role

--- frontend/src/auth/AuthProvider.tsx
+++ frontend/src/auth/AuthProvider.tsx
@@ -8,6 +8,7 @@ import type { AuthContextType } from './auth.types.ts';
 const STORAGE_KEYS = {
   token: 'auth_token',
   email: 'auth_email',
+  userRole: 'user_role',
 };
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
@@ -22,6 +23,11 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     return localStorage.getItem(STORAGE_KEYS.email);
   });
 
+  const [userRole, setUserRole] = useState<number | null>(() => {
+    const value = localStorage.getItem(STORAGE_KEYS.userRole);
+    if (value != null) return parseInt(value); else return 0;
+  });
+  
   const isLoggedIn = !!token;
 
   useEffect(() => {
@@ -32,31 +38,38 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const timer = window.setTimeout(() => {
       setToken(null);
       setUserEmail(null);
+      setUserRole(0);
       localStorage.removeItem(STORAGE_KEYS.token);
       localStorage.removeItem(STORAGE_KEYS.email);
+      localStorage.removeItem(STORAGE_KEYS.userRole);
     }, remaining);
 
     return () => window.clearTimeout(timer);
   }, [token]);
 
-  const login = (email: string, newToken: string) => {
+  const login = (email: string, newToken: string, role: number) => {
     if (getTokenRemainingTime(newToken) <= 0) return;
 
     setToken(newToken);
     setUserEmail(email);
-
+    setUserRole(role);
+    
     localStorage.setItem(STORAGE_KEYS.token, newToken);
     localStorage.setItem(STORAGE_KEYS.email, email);
+    localStorage.setItem(STORAGE_KEYS.userRole, role.toString());
   };
 
   const logout = () => {
     setToken(null);
     setUserEmail(null);
+    setUserRole(0);
     localStorage.removeItem(STORAGE_KEYS.token);
     localStorage.removeItem(STORAGE_KEYS.email);
+    localStorage.removeItem(STORAGE_KEYS.userRole);
   };
 
   const value: AuthContextType = {
+    userRole,
     isLoggedIn,
     token,
     userEmail,
